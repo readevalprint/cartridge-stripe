64318065c0cb818fe24f4ef1499854f98592dfd7
Tim Watts <tim@readevalprint.com>
2013-06-06 15:02:35 +0000

    Fix MANIFEST and setup.py to include templates, updated template for Django 1.5

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from distutils.core import setup
+from setuptools import find_packages
+
 
 setup(
     name='cartridge-stripe',
-    version='0.1.1',
+    version='0.1.3',
     author='Timothy Watts (readevalprint)',
     author_email='tim@readevalprint.com',
-    packages=['cartridge_stripe'],
+    packages=find_packages(),
     url='https://github.com/readevalprint/cartridge-stripe/',
     license='LICENSE.txt',
+    include_package_data=True,
     description='Stripe credit card processing integration with Cartridge',
     long_description=open('README.rst').read(),
 )
